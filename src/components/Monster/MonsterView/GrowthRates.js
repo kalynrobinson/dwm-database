4720564464abe0d09252c266fbe2a3f702816c4a
Kalyn Robinson <kalyndrobinson@gmail.com>
2018-12-06 12:13:32 -0500

    Improve MonsterView styles, accessibility

--- src/components/Monster/MonsterView/GrowthRates.js
+++ src/components/Monster/MonsterView/GrowthRates.js
@@ -1,28 +1,39 @@
-import React from "react"
-import { Card, Icon, Table, Tooltip } from "antd"
+import React from "react";
+import { Card, Icon, Table, Tooltip } from "antd";
 
-import { StatAPI } from "api"
-import type { Monster } from "types"
+import { StatAPI } from "api";
+import type { Monster } from "types";
 
-type DataEntry = { stat: string, value: number }
-type Props = { monster: Monster }
+type DataEntry = { stat: string, value: number };
+type Props = { monster: Monster };
 
 /** Stats that are "growth" stats, i.e. excluding SX, FL, and ME */
-const growthStats: string[] = ["Max LVL", "EXP", "HP", "MP", "ATK", "DEF", "AGI", "INT"]
+const growthStats: string[] = [
+    "Max LVL",
+    "EXP",
+    "HP",
+    "MP",
+    "ATK",
+    "DEF",
+    "AGI",
+    "INT",
+];
 
 /**
  * Displays monster's max level and growth rates in a data table.
  */
 const GrowthRates = ({ monster }: Props) => {
     const growthRates: DataEntry = Object.entries(monster.stats).reduce(
-        (acc, [stat, value]) => (growthStats.indexOf(stat) >= 0 ? [...acc, { stat, value }] : acc),
+        (acc, [stat, value]) =>
+            growthStats.indexOf(stat) >= 0 ? [...acc, { stat, value }] : acc,
         [],
-    )
+    );
 
     const columns = [
         {
             dataIndex: "stat",
             key: "growth-stat",
+            className: "text-dark",
             render: (value) => (
                 <Tooltip placement="topLeft" title={StatAPI.get(value).name}>
                     <Icon type="question-circle" className="mr-2" />
@@ -34,13 +45,20 @@ const GrowthRates = ({ monster }: Props) => {
             dataIndex: "value",
             key: "growth-value",
         },
-    ]
+    ];
 
     return (
-        <Card title="Growth Rates" bordered={false} className="ant-card--hide-thead">
-            <Table dataSource={growthRates} pagination={false} columns={columns} size="medium" rowKey="stat" />
+        <Card title="Growth Rates" bordered={false}>
+            <Table
+                dataSource={growthRates}
+                pagination={false}
+                columns={columns}
+                size="medium"
+                rowKey="stat"
+                onHeaderRow={(col, ndx) => ({ className: "sr-only" })}
+            />
         </Card>
-    )
-}
+    );
+};
 
-export default GrowthRates
+export default GrowthRates;
